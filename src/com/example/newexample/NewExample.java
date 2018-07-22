626a44e2af81ac816d9e8ca650ef78db1cb3650c
Christian <christiantisby1@gmail.com>
2018-07-22 10:07:56 -0400

    completed project

---
+++ src/com/example/newexample/NewExample.java
@@ -0,0 +1,19 @@
+package com.example.newexample;
+
+/*
+
+    This is a simple Java Program.
+
+    Call this file NewExample.java
+
+ */
+
+public class NewExample {
+    //  A java program begins with a call to main().
+    public static void main(String args[]) {
+        System.out.println("Java drives the Web.");
+        System.out.println("I'm determined to learn java!");
+        System.out.println("Hello to a new career!");
+
+    }
+}
